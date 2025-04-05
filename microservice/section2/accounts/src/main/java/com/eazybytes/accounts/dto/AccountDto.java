53d9e20d91ff80be4940e2b41e93649c083c28a6
nguyenhoangthaituandev <nguyenhoangthaituandev@gmail.com>
2025-04-05 22:48:35 +0700

    18 create dto inside account microservice

---
+++ microservice/section2/accounts/src/main/java/com/eazybytes/accounts/dto/AccountDto.java
@@ -0,0 +1,14 @@
+package com.eazybytes.accounts.dto;
+
+
+import lombok.Data;
+
+@Data
+public class AccountDto {
+
+    private Long accountNumber;
+
+    private String accountType;
+
+    private String branchAddress;
+}
