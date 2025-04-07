fa7a0b08ada8b280c289d07ca698513373a9884d
nguyenhoangthaituandev <nguyenhoangthaituandev@gmail.com>
2025-04-07 16:47:12 +0700

    [27] config openapi swagger

---
+++ microservice/section2/accounts/src/main/java/com/eazybytes/accounts/config/OpenAPIConfig.java
@@ -0,0 +1,19 @@
+package com.eazybytes.accounts.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenAPIConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Accounts Service API")
+                        .version("1.0")
+                        .description("Swagger UI cấu hình thủ công cho microservice Accounts"));
+    }
+}
