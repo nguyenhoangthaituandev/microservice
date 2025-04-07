aace15dba84edb95adb50881a0b5be8a8a0fe290
nguyenhoangthaituandev <nguyenhoangthaituandev@gmail.com>
2025-04-07 17:15:32 +0700

    [28] Separate the openapi file instead of merging it into the main application file.

--- microservice/section2/accounts/src/main/java/com/eazybytes/accounts/AccountsApplication.java
+++ microservice/section2/accounts/src/main/java/com/eazybytes/accounts/AccountsApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+/*@ComponentScans({ @ComponentScan("com.eazybytes.accounts.controller") })
+@EnableJpaRepositories("com.eazybytes.accounts.repository")
+@EntityScan("com.eazybytes.accounts.model")*/
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
 public class AccountsApplication {
 
--- microservice/section2/accounts/src/main/java/com/eazybytes/accounts/config/OpenAPIConfig.java
+++ microservice/section2/accounts/src/main/java/com/eazybytes/accounts/config/OpenAPIConfig.java
@@ -1,7 +1,10 @@
 package com.eazybytes.accounts.config;
 
+import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,8 +15,18 @@ public class OpenAPIConfig {
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .info(new Info()
-                        .title("Accounts Service API")
-                        .version("1.0")
-                        .description("Swagger UI cấu hình thủ công cho microservice Accounts"));
+                        .title("Accounts microservice REST API Documentation")
+                        .description("This is demo REST API for Accounts microservice")
+                        .version("v1")
+                        .contact(new Contact()
+                                .name("Nguyen Hoang Thai Tuan")
+                                .email("nguyenhoangthaituandev@gmail.com")
+                                .url("https://www.eazybytes.com"))
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("https://www.eazybytes.com")))
+                .externalDocs(new ExternalDocumentation()
+                        .description(" Accounts microservice REST API Documentation")
+                        .url("https://www.eazybytes.com/swagger-ui.html"));
     }
 }
